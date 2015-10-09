5bfc711cc043eb2925526e8732c5e696b4ad360e
Colin Cruse <colin.cruse@gmail.com>
2015-10-09 13:55:14 -0500

    Fixed going to login from MainActivty
    used to crash, doesnt now

--- app/src/main/java/com/csc394/gsp/gradschoolplanner/MainActivity.java
+++ app/src/main/java/com/csc394/gsp/gradschoolplanner/MainActivity.java
@@ -2,19 +2,11 @@ package com.csc394.gsp.gradschoolplanner;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.PopupWindow;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -68,6 +60,7 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
+        //go to appropriate activities if selected
         if (id == R.id.action_settings) {
             //open settings
             Intent intent = new Intent(getApplicationContext(),SettingsActivity.class);
