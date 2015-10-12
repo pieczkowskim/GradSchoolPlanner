73c23fa18aabef3bf7b32783530821a9516c138d
Colin Cruse <colin.cruse@gmail.com>
2015-10-12 18:51:39 -0500

    Fixed crash when clicking for course desc.
    Changed all appcompatactivity to activity, because I am dumb.

--- app/src/main/java/com/csc394/gsp/gradschoolplanner/CourseInfo.java
+++ app/src/main/java/com/csc394/gsp/gradschoolplanner/CourseInfo.java
@@ -1,5 +1,6 @@
 package com.csc394.gsp.gradschoolplanner;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,7 +10,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class CourseInfo extends AppCompatActivity {
+public class CourseInfo extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
